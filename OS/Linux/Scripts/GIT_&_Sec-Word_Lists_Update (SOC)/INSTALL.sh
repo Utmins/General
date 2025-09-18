d87dceb5df9a0e74d9d2f1ddb15a7537bef5f0c6
Du8ePrehT <120234987+Utmins@users.noreply.github.com>
2025-09-18 11:39:36 -0700

    Update INSTALL.sh

--- OS/Linux/Scripts/GIT_&_Sec-Word_Lists_Update (SOC)/INSTALL.sh
+++ OS/Linux/Scripts/GIT_&_Sec-Word_Lists_Update (SOC)/INSTALL.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-SCRIPT_NAME="Purple_GIT_&_Sec-Word_Lists_Update_v8.sh"
+SCRIPT_NAME="Purple_GIT_&_Sec-Word_Lists_Update.sh"
 TARGET="${1:-/usr/local/bin}"
 echo "[+] Installing $SCRIPT_NAME to $TARGET (sudo may be required)"
 sudo install -m 0755 "$SCRIPT_NAME" "$TARGET/$SCRIPT_NAME"
